d63d2164100bd6f9f0dc77bbc1e658e51be11bcc
Dmit Bashmakov <denegniy1@gmail.com>
2024-11-20 14:25:29 +0500

    added CRUD for habits, basic pagination and serializer

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from habits.models import Habit
+from habits.paginators import CustomPagination
+from rest_framework import generics
 
-# Create your views here.
+from habits.serializers import HabitSerializer
+
+
+class HabitListAPIView(generics.ListAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    pagination_class = CustomPagination
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    # permission_classes = (IsAuthenticated, IsOwner)
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    # permission_classes = (IsAuthenticated)
+
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.created_by = self.request.user
+        habit.save()
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    # permission_classes = (IsAuthenticated, IsOwner)
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    # permission_classes = (IsAuthenticated, IsOwner)
