17d7b650ff4dec971837313877b5e0fa63c22697
Dmit Bashmakov <denegniy1@gmail.com>
2024-11-20 11:11:15 +0500

    added validation for habits

--- habits/models.py
+++ habits/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from users.models import User
 
@@ -31,10 +32,16 @@ class Habit(models.Model):
     is_public = models.BooleanField(default=False, verbose_name='Публичность')
 
     def __str__(self):
-        return f'{self.created_by.username} - {self.action}'
+        return f'{self.created_by} - {self.action}'
+
+    def clean(self):
+        super().clean()
+        if self.is_pleasant and self.connected:
+            raise ValidationError('Привычка может быть или приятной, или иметь связанную приятную.')
+        if self.connected and self.reward:
+            raise ValidationError('Привычка может иметь или связанную приятную, или награду!')
 
     class Meta:
         verbose_name = 'Привычка'
         verbose_name_plural = 'Привычки'
         ordering = ['-date']
-
