2d8a9e2e0af5f721fa06b6dd9ca5c96b936de0ee
Dmit Bashmakov <denegniy1@gmail.com>
2024-11-21 16:36:13 +0500

    added shared task for checking required notifications

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,11 +1,16 @@
 from celery import shared_task
 from django.utils import timezone
 
-from habits.services import send_telegram_message
+from habits.models import Habit
+from habits.services import send_notification
 
 
 @shared_task
 def check_habit_notifications():
-    today = timezone.now()
-    print(today)
+    current_date = timezone.now().date()
+    current_time = timezone.now().time()
+    for habit in Habit.objects.filter(date=current_date, time=current_time):
+        send_notification(habit.created_by.tg_chat_id, habit)
 
+        if habit.date:
+            habit.date = habit.date + timezone.timedelta(days=habit.period)
